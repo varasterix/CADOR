cfab03cab68c80bb67209d13542dad24104ab23a
Emilie Lhomme <emilie.lhomme@laposte.net>
2020-02-26 16:24:20 +0100

    Some parameters are added to the model

--- work_cycles.py
+++ work_cycles.py
@@ -2,6 +2,14 @@ from pulp import *
 from time import time
 
 # Parameters
+Week = [i for i in range(7)]                            # days of week
+T = [i for i in range(7)]                               # types of contracts
+Shifts = {                                              # types of shifts
+    "Morning": {"code": "M", "beginningTime_t": 6, "duration_D": 7.5, "completionTime_c": 14, "breakDuration": 0.5},
+    "Day": {"code": "J", "beginningTime_t": 9, "duration_D": 7.5, "completionTime_c": 17, "breakDuration": 0.5},
+    "Evening": {"code": "S", "beginningTime_t": 14, "duration_D": 7.5, "completionTime_c": 22, "breakDuration": 0.5},
+    "Night": {"code": "N", "beginningTime_t": 20, "duration_D": 10, "completionTime_c": 6, "breakDuration": None},
+}
 
 # Instance dependant Parameters
 
