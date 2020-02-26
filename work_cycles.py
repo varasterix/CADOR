b994656c922d13fd54c130473d7254145a03b701
Emilie Lhomme <emilie.lhomme@laposte.net>
2020-02-26 16:51:23 +0100

    Instance dependant Parameters are added

--- work_cycles.py
+++ work_cycles.py
@@ -1,17 +1,26 @@
 from pulp import *
+import numpy as np
 from time import time
 
 # Parameters
 Week = [i for i in range(7)]                            # days of week
 T = [i for i in range(7)]                               # types of contracts
-Shifts = {                                              # types of shifts
-    "Morning": {"code": "M", "beginningTime_t": 6, "duration_D": 7.5, "completionTime_c": 14, "breakDuration": 0.5},
-    "Day": {"code": "J", "beginningTime_t": 9, "duration_D": 7.5, "completionTime_c": 17, "breakDuration": 0.5},
-    "Evening": {"code": "S", "beginningTime_t": 14, "duration_D": 7.5, "completionTime_c": 22, "breakDuration": 0.5},
-    "Night": {"code": "N", "beginningTime_t": 20, "duration_D": 10, "completionTime_c": 6, "breakDuration": None},
-}
+Night_Shifts = ["N"]                                    # types of night shifts
+Day_Shifts = ["M", "J", "S"]                            # types of day shifts
+Off_Shifts = ["Jca", "Repos"]                           # types of off shifts
 
 # Instance dependant Parameters
+beginningTime_t = {"M": 6, "J": 9, "S": 14, "N": 20}
+completionTime_c = {"M": 14, "J": 17, "S": 22, "N": 6}
+duration_D = {"M": 7.5, "J": 7.5, "S": 7.5, "N": 10}
+breakDuration = {"M": 0.5, "J": 0.5, "S": 0.5}
+N = [{"M": 2, "J": 1, "S": 2, "N": 1} for j in Week]  # workforce Needs for every shifts of every day in week
+Eff = [3 for i in T]                                  # number of employees already affected for each type of contract
+# Work cycles length
+HC_r = [eff for eff in Eff]
+# Overall work cycle length
+HC = int(np.lcm.reduce(HC_r))
+# Horizon of the plannings creation
 
 # Variables
 
