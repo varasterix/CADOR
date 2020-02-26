52b3fbbcce291ccb7a8412c4a9495bc89a2b9971
carlivoix <charlesvivoix@gmail.com>
2020-02-26 16:55:37 +0100

    work cycles variables

--- work_cycles.py
+++ work_cycles.py
@@ -14,8 +14,8 @@ beginningTime_t = {"M": 6, "J": 9, "S": 14, "N": 20}
 completionTime_c = {"M": 14, "J": 17, "S": 22, "N": 6}
 duration_D = {"M": 7.5, "J": 7.5, "S": 7.5, "N": 10}
 breakDuration = {"M": 0.5, "J": 0.5, "S": 0.5}
-N = [{"M": 2, "J": 1, "S": 2, "N": 1} for j in Week]  # workforce Needs for every shifts of every day in week
-Eff = [3 for i in T]                                  # number of employees already affected for each type of contract
+N = [{"M": 2, "J": 1, "S": 2, "N": 1} for j in Week]    # workforce Needs for every shifts of every day in week
+Eff = [3 for i in T]                                    # number of employees already affected for each type of contract
 # Work cycles length
 HC_r = [eff for eff in Eff]
 # Overall work cycle length
@@ -25,7 +25,7 @@ HC = int(np.lcm.reduce(HC_r))
 # Variables
 X = [[[LpVariable("x"+str(i)+"_"+str(j)+"_"+str(r), 0, 1, LpInteger)
        for i in range(4)] for j in range(7)] for r in range(7)]
-HCR = [LpVariable("Hc"+str(i), 0, cat=LpInteger) for i in range(7)]
+HCR = [LpVariable("Hc"+str(i), 0, cat=LpInteger) for i in T]
 
 # Problem
 cador = LpProblem("CADOR", LpMinimize)
