8b95786b0eba2cdbd1b7c42e9f8650f72dd603f8
Pancake39 <emilie.lhomme@laposte.net>
2020-03-11 16:02:31 +0100

    Modification of the file team_composition.py

--- team_composition.py
+++ team_composition.py
@@ -2,38 +2,46 @@ from pulp import *
 from time import time
 
 # Parameters
-Week = [i for i in range(7)]                            # days of week
-T = [i for i in range(7)]                               # types of contracts
-ratios = [1, 0.9, 0.8, 0.75, 0.7, 0.6, 0.5]             # ratio of each type of contract
-costs = [1, 0.9143, 0.8571, 0.75, 0.7, 0.6, 0.5]        # cost of each type of contract
+Week = [i for i in range(7)]  # days of week
+T = [i for i in range(1, 8)]  # types of contracts
+ratios = [1, 0.9, 0.8, 0.75, 0.7, 0.6, 0.5]  # ratio of each type of contract
+costs = [1, 0.9143, 0.8571, 0.75, 0.7, 0.6, 0.5]  # cost of each type of contract
 
 # Instance dependant Parameters
-bw = 20                                                 # budgeted workforce
-N = [[2, 2] for j in Week]                              # workforce Needs for every day and night of every day in week
-A = [100 for i in T]                                    # availability for every type of contract
-Pp = 0.3                                                # proportion of partial time contracts
-P80 = 0.2                                               # proportion of 80% contracts in total partial contracts
-a = [0 for i in T]                                      # number of employees already affected for each type of contract
+bw = 20  # budgeted workforce
+N = [{"M": 2, "J": 1, "S": 2, "N": 1} for j in Week]  # workforce needs for every shifts of every day in week
+A = [100 for i in T]  # availability for every type of contract
+Pp = 0.3  # proportion of partial time contracts
+P80 = 0.2  # proportion of 80% contracts in total partial contracts
+a = [0 for i in T]  # number of employees already affected for each type of contract
 
 # Variables
-W = [LpVariable("W"+str(i), 0, cat=LpInteger) for i in T]     # workforce of every type of contract
+W = [LpVariable("W"+str(i), 0, cat=LpInteger) for i in T]  # workforce of every type of contract
 
 # Problem
 cador = LpProblem("CADOR", LpMinimize)
 
 # Constraints
-cador += lpSum([W[i] * ratios[i] for i in T]) == bw     # budgeted workforce respected
+
+# Budgeted workforce respected
+cador += lpSum([W[i - 1] * ratios[i - 1] for i in T]) == bw
+
+# Availability respected
 for i in T:
-    cador += W[i] <= a[i] + A[i]                        # availability respected
-cador += lpSum([W[i] * ratios[i] for i in T]) >= \
-         2 * (N[6][0] + N[6][1])                        # employees cannot work two sundays in a row
-cador += lpSum([W[i] for i in range(1, 7)]) >= \
-         Pp * lpSum([W[i] for i in T])                  # ratio of partial time contracts
-cador += W[2] >= \
-         P80 * lpSum([W[i] for i in range(1, 7)])       # ratio of 80% contracts in partial time contracts
+    cador += W[i - 1] <= a[i - 1] + A[i - 1]
+
+# Employees cannot work two sundays in a row
+cador += lpSum([W[i - 1] * ratios[i - 1] for i in T]) >= 2 * sum([N[6][shift] for shift in N[6]])
+
+# Ratio of partial time contracts
+cador += lpSum([W[i - 1] for i in range(2, 8)]) >= Pp * lpSum([W[i - 1] for i in T])
+
+# Ratio of 80% contracts in partial time contracts
+cador += W[2] >= P80 * lpSum([W[i - 1] for i in range(2, 8)])
 
 # Target Function
-cador += lpSum([W[i] * costs[i] for i in T]) - W[0]     # minimization of the global cost and maximization of full time
+# Minimization of the global cost and maximization of full time
+cador += lpSum([W[i - 1] * costs[i - 1] for i in T]) - W[0]
 
 # Solving
 start_time = time()
