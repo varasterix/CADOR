83a383445767f289782503f3adfe762145f15ef8
Pancake39 <emilie.lhomme@laposte.net>
2020-03-11 22:27:47 +0100

    team_composition_model.py modified + some functions are added in src/utils.py

--- src/utils.py
+++ src/utils.py
@@ -101,9 +101,9 @@ def read_team_composition_data_from_csv(file_path):
                 needs_by_day[s] = needs_by_shifts[s][day]
             needs_by_days.append(needs_by_day)
         csv_file.close()
-    return instance_id, budgeted_workforce, partial_time_contracts_prop, eighty_percent_contracts_prop, \
-           contracts_type, contracts_ratios, contracts_costs, contracts_availability, contracts_affected, week_days, \
-           week_indices, needs_by_days, shifts
+    return (instance_id, budgeted_workforce, partial_time_contracts_prop, eighty_percent_contracts_prop,
+            contracts_type, contracts_ratios, contracts_costs, contracts_availability, contracts_affected, week_days,
+            week_indices, needs_by_days)
 
 
 def export_team_composition_results_as_csv(exportation_repository_path, instance_id, status, solving_time,
@@ -113,31 +113,156 @@ def export_team_composition_results_as_csv(exportation_repository_path, instance
         writer = csv.writer(csv_file, delimiter=';', lineterminator='\n')
         instance_row = ['instance_id', str(instance_id)]
         time_row = ['solving_time', str(solving_time)]
+        status_row = ['status', status]
+        all_rows = [instance_row, time_row, status_row]
         if status == 'Optimal':
-            types_row = [str(t) for t in contract_types]
-            ratios_row = [str(r_t) for r_t in contract_ratios]
-            workforce_row = [str(w) for w in workforce]
-            all_rows = [instance_row, time_row, types_row, ratios_row, workforce_row]
-        else:
-            status_row = ['status', status]
-            all_rows = [instance_row, time_row, status_row]
+            all_rows.append([str(t) for t in contract_types])
+            all_rows.append([str(r_t) for r_t in contract_ratios])
+            all_rows.append([str(w) for w in workforce])
         writer.writerows(all_rows)
     csv_file.close()
 
 
-def read_team_composition_results_as_csv(exportation_repository_path, instance_id):
-    file_path = exportation_repository_path + "team_composition_" + str(instance_id)
+def read_team_composition_results(exportation_repository_path, instance_id):
+    file_path = exportation_repository_path + "team_composition_" + str(instance_id) + ".csv"
+    with open(file_path, 'r') as csv_file:
+        reader = csv.reader(csv_file, delimiter=';')
+        for index, row in enumerate(reader):
+            if index == 2:
+                status = row[1]
+                if status != 'Optimal':
+                    raise Exception('The status after solving the team composition ({}) phase is un-managed'
+                                    .format(status))
+            elif index == 5:
+                workforce_by_contract_types = [int(w) for w in row if w != '']
+    csv_file.close()
+    return workforce_by_contract_types
+
+
+def read_planning_data_from_csv(file_path):
+    """
+    Note: The planning csv data file has to respect a special format
+    :param file_path: path of the file containing the planning data
+    :return:    - instance id of the team composition data
+                - budgeted workforce
+                - proportion of partial time contracts
+                - proportion of 80% contracts in total partial contracts
+                - types of contracts
+                - ratio of each type of contract
+                - cost of each type of contract
+                - availability for every type of contract
+                - number of employees already affected for each type of contract
+                - types of day shifts
+                - types of night shifts
+                - types of off shifts
+                - day codes (str) of the days of a week
+                - indices (int) of the days of a week
+                - workforce needs for every shifts of every day in week
+                - beginning times of some shifts
+                - completion times of some shifts
+                - durations of some shifts
+                - break durations of some shifts
+    """
+    shifts_with_needs, shift_index = [], 0
+    needs_by_shifts = {}
+    shift_beginning_times, shift_completion_times, shift_durations, shift_break_durations = {}, {}, {}, {}
+    with open(file_path, 'r') as csv_file:
+        reader = csv.reader(csv_file, delimiter=';')
+        for index, row in enumerate(reader):
+            if index == 0:
+                instance_id = int(row[1])
+            elif index == 1:
+                budgeted_workforce = float(row[1])
+            elif index == 2:
+                partial_time_contracts_prop = float(row[1])
+            elif index == 3:
+                eighty_percent_contracts_prop = float(row[1])
+            elif index == 4:
+                contracts_type = [int(row[i]) for i in range(1, 8)]
+            elif index == 5:
+                contracts_ratios = [float(row[i]) for i in range(1, 8)]
+            elif index == 6:
+                contracts_costs = [float(row[i]) for i in range(1, 8)]
+            elif index == 7:
+                contracts_availability = [int(row[i]) for i in range(1, 8)]
+            elif index == 8:
+                contracts_affected = [int(row[i]) for i in range(1, 8)]
+            elif index == 9:
+                day_shifts, i = {}, 1
+                while row[i] != '':
+                    day_shifts[row[i]] = shift_index
+                    shift_index += 1
+                    i += 1
+            elif index == 10:
+                night_shifts, i = {}, 1
+                while row[i] != '':
+                    night_shifts[row[i]] = shift_index
+                    shift_index += 1
+                    i += 1
+            elif index == 11:
+                off_shifts, i = {}, 1
+                while row[i] != '':
+                    off_shifts[row[i]] = shift_index
+                    shift_index += 1
+                    i += 1
+            elif index == 12:
+                week_days = [row[i] for i in range(1, 8)]
+            elif index == 13:
+                week_indices = [int(row[i]) for i in range(1, 8)]
+            elif index >= 14:
+                shift_id = row[0]
+                end_reading = (shift_id == '')
+                if not end_reading:
+                    shifts_with_needs.append(shift_id)
+                    needs_by_shift = {}
+                    for j, day in enumerate(week_days):
+                        needs_by_shift[day] = int(row[j + 1])
+                    needs_by_shifts[shift_id] = needs_by_shift
+                    shift_beginning_times[shift_id] = int(row[8])
+                    shift_completion_times[shift_id] = int(row[9])
+                    shift_durations[shift_id] = int(row[10])
+                    shift_break_durations[shift_id] = None if len(row) < 12 or row[11] == '' else float(row[11])
+                else:
+                    break
+        needs_by_days = []
+        for day in week_days:
+            needs_by_day = {}
+            for s in shifts_with_needs:
+                needs_by_day[s] = needs_by_shifts[s][day]
+            needs_by_days.append(needs_by_day)
+        csv_file.close()
+    return (instance_id, budgeted_workforce, partial_time_contracts_prop, eighty_percent_contracts_prop,
+            contracts_type, contracts_ratios, contracts_costs, contracts_availability, contracts_affected, day_shifts,
+            night_shifts, off_shifts, week_days, week_indices, needs_by_days, shift_beginning_times,
+            shift_completion_times, shift_durations, shift_break_durations)
+
+
+def export_work_cycles_results_as_csv(exportation_repository_path, instance_id, status, solving_time,
+                                      contract_ratios, week_days, work_cycles):
+    file_path = exportation_repository_path + "work_cycles_" + str(instance_id) + ".csv"
     with open(file_path, 'w+') as csv_file:
         writer = csv.writer(csv_file, delimiter=';', lineterminator='\n')
         instance_row = ['instance_id', str(instance_id)]
         time_row = ['solving_time', str(solving_time)]
+        status_row = ['status', status]
+        all_rows = [instance_row, time_row, status_row]
         if status == 'Optimal':
-            types_row = [str(t) for t in contract_types]
-            ratios_row = [str(r_t) for r_t in contract_ratios]
-            workforce_row = [str(w) for w in workforce]
-            all_rows = [instance_row, time_row, types_row, ratios_row, workforce_row]
-        else:
-            status_row = ['status', status]
-            all_rows = [instance_row, time_row, status_row]
+            # Labels of the work cycles table
+            horizon = len(work_cycles[0][0])
+            nb_of_weeks = int(horizon / 7)
+            legend_row_1 = ['Prénom', '%', 'J/N']
+            legend_row_2 = ['Agent', '%', 'J/N']
+            for week in range(nb_of_weeks):
+                legend_row_1 += ['Semaine {}'.format(week + 1)] + ['' for _ in range(6)]
+                legend_row_2 += week_days
+            all_rows.append(legend_row_1)
+            all_rows.append(legend_row_2)
+            # Work cycles table content
+            agent = 1
+            for r in range(len(work_cycles)):
+                for e_r in range(len(work_cycles[r])):
+                    row_r_er = [shift for shift in work_cycles[r][e_r]]
+                    all_rows.append([str(agent), str(int(contract_ratios[r] * 100)), ''] + row_r_er)
+                    agent += 1
         writer.writerows(all_rows)
     csv_file.close()
