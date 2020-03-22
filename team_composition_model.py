e776c61da65db5acabde239bfa06ca4f2dc6c48a
Pancake39 <emilie.lhomme@laposte.net>
2020-03-22 09:21:25 +0100

    A single file is used to read the planning information (planning data file) in both models team_composition and work_cycles

--- team_composition_model.py
+++ team_composition_model.py
@@ -1,15 +1,15 @@
 from pulp import *
 import time
-from src.utils import read_team_composition_data_from_csv, export_team_composition_results_as_csv
+from src.utils import read_planning_data_from_csv, export_team_composition_results_as_csv
 
 # Loading team composition csv data file
-data_file_path = sys.argv[1]
+planning_data_file_path = sys.argv[1]
 exportation_path = sys.argv[2]
 export_results = bool(int(sys.argv[3]))
 
 # Parameters + Instance dependant Parameters
-instance_id, bw, Pp, P80, T, ratios, costs, A, a, week_days, week_indices, N = \
-    read_team_composition_data_from_csv(data_file_path)
+instance_id, bw, Pp, P80, T, ratios, costs, A, a, Day_Shifts, Night_Shifts, Off_Shifts, week_days, Week, N, \
+    beginningTime_t, completionTime_c, duration_D, breakDuration = read_planning_data_from_csv(planning_data_file_path)
 
 # Variables
 W = [LpVariable("W"+str(i), 0, cat=LpInteger) for i in T]  # workforce of every type of contract
